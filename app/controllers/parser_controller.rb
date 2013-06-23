73b5269e8eb98db2a79206609e0a8f8571712069
just3obad <just3obad@gmail.com>
2013-06-23 16:12:15 +0200

    cleaning up

--- app/controllers/parser_controller.rb
+++ app/controllers/parser_controller.rb
@@ -11,21 +11,6 @@ class ParserController < ApplicationController
 		if(@answer.length == 0)
 			@answer = " Unknown "
 		end
-		render :file=>"parser/result.vxml.erb", :content_type => 'application/vxml'
-	end
-
-	def get_html_xml
-		keyword = params[:keyword]
-		type = params[:type]
-		@answer = Parser.get_html(keyword,type)
-		if(@answer.length == 0)
-			@answer = " Unknown "
-		end
-		render :file=>"parser/test.xml.erb", :content_type => 'application/xml'
-	end
-
-	def test
-		render :file=>"parser/test.xml.erb", :content_type => 'application/xml'
-		# render :text => "Test success"
+		render :file=>"parser/result.xml.erb", :content_type => 'application/xml'
 	end
 end
