150f2a935e9dc592abf4a08805d3796da4d538b0
just3obad <just3obad@gmail.com>
2013-07-05 03:25:52 +0200

    OpenEphyra Intergration

--- app/models/parser.rb
+++ app/models/parser.rb
@@ -6,13 +6,22 @@ class Parser < ActiveRecord::Base
   attr_accessible :title, :body
 
   def self.get_html(keyword,type)
-  	page = Nokogiri::HTML(open("http://it-tjr10.dhbw-stuttgart.de/home/?keyword=#{keyword}&q_type=#{type}"))
-  	return page.css('div#answer').text
+  	begin
+  		page = Nokogiri::HTML(open("http://it-tjr10.dhbw-stuttgart.de/home/?keyword=#{keyword}&q_type=#{type}"))
+  		return page.css('div#answer').text
+  	rescue
+  		return "No answer"
+  	end
   end
 
   def self.dummy_get
-  	page = Nokogiri::HTML(open("http://it-tjr2.dhbw-stuttgart.de:8080/hello/echo?question=Who+discovered+Oxygen%3F%0D%0A&secret=888"))
-  	return page.css('div#answer').text
+  	begin
+  		page = Nokogiri::HTML(open("http://it-tjr2.dhbw-stuttgart.de:8080/hello/echo?question=Who+discovered+Oxygen%3F%0D%0A&secret=888"))
+  		return page.css('div#answer').text
+  	rescue
+  		return "No answer"
+  	end
+
   end
 
 end
